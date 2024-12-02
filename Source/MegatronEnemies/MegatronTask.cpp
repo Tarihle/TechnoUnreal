c96358496e1353613076b27b5b2990b18ad5f8fb
Noah DE PISCHOF <n.depischof@student.isartdigital.com>
2024-12-02 18:20:54 -0500

    Create MegatronTask class
    Create class with overridable Execute and Tick functions similar to a BTTask

---
+++ Source/MegatronEnemies/MegatronTask.cpp
@@ -0,0 +1,57 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MegatronTask.h"
+#include "GameplayTagsManager.h"
+
+void UMegatronTask::Execute_Implementation()
+{
+	Status = EMegatronTaskStatus::IN_PROGRESS;
+}
+
+void UMegatronTask::Tick_Implementation(float DeltaSeconds)
+{
+	// Empty for parent class
+}
+
+void UMegatronTask::Abort()
+{
+	Status = EMegatronTaskStatus::ABORTED;
+	OnTaskAbort.Broadcast();
+}
+
+
+EMegatronTaskStatus UMegatronTask::GetStatus() const
+{
+	return Status;
+}
+
+void UMegatronTask::Finish(bool bSuccess)
+{
+	if (bSuccess)
+	{
+		Status = EMegatronTaskStatus::COMPLETED;
+		OnTaskSuccess.Broadcast();
+	}
+	else
+	{
+		Status = EMegatronTaskStatus::FAILED;
+		OnTaskFailure.Broadcast();
+	}
+}
+
+EMegatronTaskStatus UMegatronTask::InternalTick(float DeltaSeconds)
+{
+	if (EMegatronTaskType::SINGLE_CALL == Type || EMegatronTaskStatus::IN_PROGRESS != Status)
+		return Status;
+
+	CurrentTickTime += DeltaSeconds;
+
+	if (CurrentTickTime < Interval)
+		return Status;
+
+	CurrentTickTime = 0.f;
+	Tick(DeltaSeconds);
+
+	return Status;
+}
---
+++ Source/MegatronEnemies/MegatronTask.h
@@ -0,0 +1,86 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "MegatronTask.generated.h"
+
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FTaskCompletedSignature);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FTaskFailedSignature);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FTaskAbortedSignature);
+
+UENUM(BlueprintType)
+enum class EMegatronTaskStatus : uint8
+{
+	NOT_STARTED,
+	IN_PROGRESS,
+	COMPLETED,
+	FAILED,
+	ABORTED,
+	IMPOSSIBLE
+};
+
+UENUM()
+enum class EMegatronTaskType : uint8
+{
+	SINGLE_CALL,
+	TICKED
+};
+
+
+/**
+ *
+ */
+UCLASS(Blueprintable, Abstract)
+class MEGATRONENEMIES_API UMegatronTask : public UObject
+{
+	GENERATED_BODY()
+
+	friend class UBodyPart;
+
+public:
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	void Execute();
+
+	UFUNCTION(BlueprintNativeEvent)
+	void Tick(float DeltaSeconds);
+
+	UFUNCTION(BlueprintCallable)
+	EMegatronTaskStatus GetStatus() const;
+
+	UFUNCTION(BlueprintCallable)
+	void Abort();
+
+	UFUNCTION(BlueprintCallable)
+	void Finish(bool bSuccess);
+
+private:
+
+	EMegatronTaskStatus InternalTick(float DeltaSeconds);
+
+public:
+
+	UPROPERTY(BlueprintReadWrite)
+	FTaskCompletedSignature		OnTaskSuccess;
+
+	UPROPERTY(BlueprintReadWrite)
+	FTaskAbortedSignature		OnTaskAbort;
+
+	UPROPERTY(BlueprintReadWrite)
+	FTaskFailedSignature		OnTaskFailure;
+
+protected:
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	float					Interval = -1.f;
+	float					CurrentTickTime = 0.f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	EMegatronTaskType		Type = EMegatronTaskType::TICKED;
+
+	EMegatronTaskStatus		Status = EMegatronTaskStatus::NOT_STARTED;
+
+};
