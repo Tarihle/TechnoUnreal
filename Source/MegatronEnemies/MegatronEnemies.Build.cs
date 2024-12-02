179b27fb2c37b86e0583b0a9857d3fd6ea0489fc
Noah DE PISCHOF <n.depischof@student.isartdigital.com>
2024-12-02 18:42:47 -0500

    Add GameplatTags to build modules
    Add "GameplayTags" to module list in project Build.cs file

--- Source/MegatronEnemies/MegatronEnemies.Build.cs
+++ Source/MegatronEnemies/MegatronEnemies.Build.cs
@@ -8,7 +8,7 @@ public class MegatronEnemies : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "SkeletalMerging" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "SkeletalMerging", "GameplayTags" });
 
         //PublicIncludePaths.AddRange(new string[] { "SkeletalMergingLibrary/Public/SkeletalMergingLibrary.h" });
     }
