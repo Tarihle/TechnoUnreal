42aa90cc82be7468015b60c9bb360f88acdb1065
Noah DE PISCHOF <n.depischof@student.isartdigital.com>
2024-12-09 16:18:07 -0500

    Add AI Senses to Perception part
    Add reference to AIPerceptionComponent and use
    it to configure senses from a Perception part
    in PerceptionPart class

--- Source/MegatronEnemies/PerceptionPart.cpp
+++ Source/MegatronEnemies/PerceptionPart.cpp
@@ -1,3 +1,151 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PerceptionPart.h"
+
+#include "PerceptionManager.h"
+#include "GameFramework/Actor.h"
+#include "GameFramework/Pawn.h"
+#include "Perception/AIPerceptionComponent.h"
+#include "Perception/AISenseConfig_Sight.h"
+#include "Perception/AISense_Hearing.h"
+#include "Perception/AISense_Sight.h"
+#include "Perception/AISense_Touch.h"
+
+
+void UPerceptionPart::InitializeComponent()
+{
+	Super::InitializeComponent();
+
+	if (APawn* PawnOwner = Cast<APawn>(GetOwner()))
+	{
+		UActorComponent* Component = PawnOwner->Controller->GetComponentByClass(UAIPerceptionComponent::StaticClass());
+		PerceptionComponent = dynamic_cast<UAIPerceptionComponent*>(Component);
+	}
+
+	checkf(PerceptionComponent, TEXT("No AI Perception Component found in owner pawn"));
+}
+
+bool UPerceptionPart::IsSenseEnabled(EPerceptionID Sense) const
+{
+	switch (Sense)
+	{
+	case EPerceptionID::SIGHT:
+		return Constraints.bCanSee;
+
+	case EPerceptionID::HEARING:
+		return Constraints.bCanHear;
+
+	case EPerceptionID::TOUCH:
+		return Constraints.bCanFeelTouch;
+
+	default: return false;
+	}
+}
+
+void UPerceptionPart::EnableSight()
+{
+	SetSenseEnabled(EPerceptionID::SIGHT, true);
+}
+
+void UPerceptionPart::DisableSight()
+{
+	SetSenseEnabled(EPerceptionID::SIGHT, false);
+}
+
+void UPerceptionPart::EnableHearing()
+{
+	SetSenseEnabled(EPerceptionID::HEARING, true);
+}
+
+void UPerceptionPart::DisableHearing()
+{
+	SetSenseEnabled(EPerceptionID::HEARING, false);
+}
+
+void UPerceptionPart::EnableTouch()
+{
+	SetSenseEnabled(EPerceptionID::TOUCH, true);
+}
+
+void UPerceptionPart::DisableTouch()
+{
+	SetSenseEnabled(EPerceptionID::TOUCH, false);
+}
+
+TArray<AActor*> UPerceptionPart::GetAllPerceivedActors() const
+{
+	return GetSensedActors(nullptr);
+}
+
+TArray<class AActor*> UPerceptionPart::GetHeardActors() const
+{
+	return GetSensedActors(UAISense_Hearing::StaticClass());
+}
+
+TArray<class AActor*> UPerceptionPart::GetSeenActors() const
+{
+	return GetSensedActors(UAISense_Sight::StaticClass());
+}
+
+TArray<class AActor*> UPerceptionPart::GetTouchedActors() const
+{
+	return GetSensedActors(UAISense_Touch::StaticClass());
+}
+
+void UPerceptionPart::SetSenseEnabled(EPerceptionID Sense, bool bEnabled)
+{
+	switch (Sense)
+	{
+	case EPerceptionID::SIGHT:
+		Constraints.bCanSee = bEnabled;
+		PerceptionComponent->SetSenseEnabled(UAISense_Sight::StaticClass(), bEnabled);
+		break;
+
+	case EPerceptionID::HEARING:
+		Constraints.bCanHear = bEnabled;
+		PerceptionComponent->SetSenseEnabled(UAISense_Hearing::StaticClass(), bEnabled);
+		break;
+
+	case EPerceptionID::TOUCH:
+		Constraints.bCanFeelTouch = bEnabled;
+		PerceptionComponent->SetSenseEnabled(UAISense_Touch::StaticClass(), bEnabled);
+		break;
+
+	default: break;
+	}
+}
+
+void UPerceptionPart::BeginPlay()
+{
+	Super::BeginPlay();
+
+	/* In UE 5.4, UpdateListener is only called if the PerceptionComponent has sense configs in
+	 its array right after construction. This might not be the case if all configs are stored
+	 in perception parts, so we update it manually to have a valid Listener ID */
+	UAIPerceptionSystem* AIPerceptionSys = UAIPerceptionSystem::GetCurrent(GetWorld());
+	if (PerceptionComponent->GetListenerId() == FPerceptionListenerID::InvalidID())
+	{
+		AIPerceptionSys->UpdateListener(*PerceptionComponent);
+	}
+
+	for (TObjectPtr<UAISenseConfig>& Config : Senses)
+	{
+		if (Config)
+			PerceptionComponent->ConfigureSense(*Config);
+	}
+
+	/*
+	 * This is called after UAIPerceptionComponent::RegisterSenseConfig when said component
+	 * has configs in its array at construction. Is it okay ? We'll see :)
+	 */
+	AIPerceptionSys->UpdateListener(*PerceptionComponent); 
+}
+
+TArray<class AActor*> UPerceptionPart::GetSensedActors(const TSubclassOf<class UAISense>& Sense) const
+{
+	TArray<AActor*> Perceived;
+	
+	PerceptionComponent->GetKnownPerceivedActors(Sense, Perceived);
+
+	return Perceived;
+}
--- Source/MegatronEnemies/PerceptionPart.h
+++ Source/MegatronEnemies/PerceptionPart.h
@@ -6,6 +6,31 @@
 #include "BodyPart.h"
 #include "PerceptionPart.generated.h"
 
+// Not in use for now.
+// TODO: Find a way to enable/disable some senses based on these variables
+USTRUCT(BlueprintType)
+struct FPerceptionConstraints
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	bool bCanSee = true;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	bool bCanHear = true;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	bool bCanFeelTouch = true;
+};
+
+UENUM(BlueprintType)
+enum class EPerceptionID : uint8
+{
+	SIGHT,
+	HEARING,
+	TOUCH
+};
+
 /**
  *
  */
@@ -13,4 +38,60 @@ UCLASS()
 class MEGATRONENEMIES_API UPerceptionPart : public UBodyPart
 {
 	GENERATED_BODY()
+
+public:
+	
+	virtual void InitializeComponent() override;
+
+	UFUNCTION(BlueprintCallable)
+	bool IsSenseEnabled(EPerceptionID Sense) const;
+	
+	UFUNCTION(BlueprintCallable, CallInEditor)
+	void EnableSight();
+	
+	UFUNCTION(BlueprintCallable, CallInEditor)
+	void DisableSight();
+
+	UFUNCTION(BlueprintCallable, CallInEditor)
+	void EnableHearing();
+	
+	UFUNCTION(BlueprintCallable, CallInEditor)
+	void DisableHearing();
+
+	UFUNCTION(BlueprintCallable, CallInEditor)
+	void EnableTouch();
+	
+	UFUNCTION(BlueprintCallable, CallInEditor)
+	void DisableTouch();
+
+	UFUNCTION(BlueprintCallable)
+	TArray<class AActor*> GetAllPerceivedActors() const;
+
+	UFUNCTION(BlueprintCallable)
+	TArray<class AActor*> GetSeenActors() const;
+
+	UFUNCTION(BlueprintCallable)
+	TArray<class AActor*> GetHeardActors() const;
+
+	UFUNCTION(BlueprintCallable)
+	TArray<class AActor*> GetTouchedActors() const;
+	
+protected:
+
+	void SetSenseEnabled(EPerceptionID Sense, bool bEnabled);
+	
+	TArray<class AActor*> GetSensedActors(const TSubclassOf<class UAISense>& Sense) const;
+
+	virtual void BeginPlay() override;
+	
+	
+	TObjectPtr<class UAIPerceptionComponent>  PerceptionComponent = nullptr;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = Behavior)
+	FPerceptionConstraints Constraints;
+
+private:
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Instanced, Category = Behavior, meta = (AllowPrivateAccess = "true"))
+	TArray<TObjectPtr<class UAISenseConfig>> Senses;
 };
